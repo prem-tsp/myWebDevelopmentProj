b7e0d9a8c8f4f5b3b5e7bfd9348b94a3063f66bb
prem-tsp <52152981+prem-tsp@users.noreply.github.com>
2019-07-29 22:07:37 +0530

    JavaScript File

--- validation.js
+++ validation.js
@@ -22,13 +22,14 @@ function checkEmail()
 {
 	var em = document.getElementById("email").value;
 	
-	var at=0,countdot=0,spl=0,blank=0;
-	for(let i = 0; i < em.length ; i++)
+	var at=0,countdot=0,spl=0,blank=0,countdot1=0,pos=0;
+	for(let i = 1; i < em.length ; i++)
 	{
 		let c= em.charAt(i);
 		if(c == '@')
 		{
 			at++;
+			 pos = i; 
 		}
 		else if(isSplCharNew(c))
 		{
@@ -38,17 +39,55 @@ function checkEmail()
 		{
 			blank++;
 		}
+		
 	}
-	if(at==1 && spl==0 && blank==0)
+	
+		for(let i =pos+1 ; i < em.length ; i++ )
+		{
+			if(c == ".")
+			{
+			countdot++;
+			}
+		}
+	
+	if(at==1 && spl==0 && blank==0 && countdot>=1 && countdot<3)
 	{
-		let check = em.lastIndexOf(".");
+		
+		let check = em.indexOf(".");
+		let check1 = em.lastIndexOf(".");
+		if(check1 == check)
+		{
 		for(let j= check+1 ; j<em.length ; j++)
 		{
-			countdot++;
+			countdot1++;
 		}
-		if(countdot==2 || countdot==3)
+		if(countdot1==2 || countdot1==3)
 			send="Valid E-mail";
-		else send="Invalid E-mail"
+		else 
+			send="Invalid E-mail";
+		}
+		else 
+		{
+			for(let j= check+1 ; j<em.length ; j++)
+			{
+			countdot1++;
+			}
+			if(countdot1!=2 || countdot1!=3)
+				send="Invalid E-mail";
+			else 
+				{
+					coundot1=0;
+					for(let j= check+1 ; j<em.length ; j++)
+					{
+						countdot1++;
+					}
+				if(countdot1==2 || countdot1==3)
+					send="Valid E-mail";
+				else 
+					send="Invalid E-mail";
+				}
+		} 
+		
 	}
 	else send="Invalid E-mail";
 
@@ -89,9 +128,11 @@ function checkPassword()
 			result1 = "Enter atleast one special Character (@,$,&,#)";
 		else result1 = "Password accepted";
 	}
-		
-	
+
 	document.getElementById("checkpass").innerHTML = result1;
+
+//	checkConfirmPassword(result1);
+	//document.getElementById("checkconfirmpass").innerHTML = send;
 }
 function isUpper(ch)
 {
@@ -120,3 +161,20 @@ function isSplCharNew(ch)
 		return true;
 	return false;
 }
+
+/*function checkConfirmPassword(result1)
+{
+	if(result1.equals("Password accepted"))
+	{
+	var send = "";
+	var pw = document.getElementById("passw").value;
+	var cpw = document.getElementById("confpassw").value;
+	if (cpw.length >= 10 && pw.equals(cpw) == true) {
+		send = "Passwords Match";
+	}
+	else send = "Passwords don't Match";
+	}
+	else send = "Incorrect";
+	document.getElementById("checkconfirmpass").innerHTML = send;
+}
+*/
