4ea934992994ecebc675bd0ac3d826c8e5fc4381
prem-tsp <52152981+prem-tsp@users.noreply.github.com>
2019-07-29 22:04:36 +0530

    JavaScript File

---
+++ EMailValidation.js
@@ -0,0 +1,92 @@
+function checkEmail()
+{
+	var em = document.getElementById("email").value;
+
+	var count = 0,lencount = 0,countdot = 0,index = 0,blank = 0;
+	var front = "";
+	var back = "";
+	var send = "";
+
+	for(let i = 0;i < em.length; i++)
+	{
+		if(em.charAt(i) == "@")
+			{
+				count++;
+				index = i;
+			}
+		else if(em.charAt(i) == " ")
+			blank++;
+	}
+	if(count == 1 && blank == 0)
+	{
+		//var atIndex = em.includes("@");
+		for(let i = 0;i < index; i++)
+		{
+			name += em.charAt(i); 
+		}
+		for(let i = index+1;i < em.length; i++)
+		{
+			back += em.charAt(i);
+			if(em.charAt(i) == ".")
+				countdot++;
+		}
+		if(countdot == 1 || countdot == 2)
+		{
+			for(let i = 0;i < back.length; i++)
+			{
+				if(isSplCharNew(back.charAt(i)))
+				{
+					send = "Invalid E-mail";
+					break;
+				}
+				if(isNumber(back.charAt(i)))
+				{
+					send = "Invalid E-mail";
+					break;
+				}
+				if(back.charAt(i) == ".")
+				{	
+					for(let j = i+1;j < back.length; j++)
+					{
+						if(back.charAt(j) != ".")
+						{
+							lencount++;
+						}
+						else break;
+					}
+					if(lencount == 2 || lencount == 3)
+					{
+						send = "Valid E-mail";
+					}
+					else {
+						send = "Invalid E-mail";
+						break;
+					} 
+					lencount = 0;
+				}
+
+			}
+		}
+		else send = "Invalid E-mail";
+	}
+	else
+		send = "Invalid E-mail";
+	
+	document.getElementById("checkemail").innerHTML = send;
+}
+
+function isSplCharNew(ch)
+{
+	var format = /[!#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]/g;
+	var em = document.getElementById("email").value;
+	
+	if(format.test(em))
+		return true;
+	return false;
+}
+function isNumber(ch)
+{
+	if(ch>='0' && ch<='9')
+		return true;
+	return false;
+}
